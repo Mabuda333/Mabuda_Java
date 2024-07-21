23dee734d43cda6ad882d1735e841ceac1e3a6b0
Mabuda <mabuda333@gmail.com>
2024-07-21 16:44:00 +0530

    java

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+public class EvenOdd {
+
+    public static void main(String [] args) {
+
+        int a=5;
+        
+        if(a%2==0){
+            System.out.println("a is a Even number");
+
+        }
+        else{
+            System.out.println("a is a Odd number");
+        }
+
+    }
+ 
+}
